1d1f331ca19f561899bb214f15a5a74887c5514c
Ajdin Imsirovic <ajdika@live.com>
2021-03-13 07:54:34 +0100

    Start the faux slider

---
+++ script.js
@@ -0,0 +1,7 @@
+let slide1 = document.getElementById('slide-1');
+let slide2 = document.getElementById('slide-2');
+let slide3 = document.getElementById('slide-3');
+
+console.log('slide1 height:', slide1.offsetHeight);
+console.log('slide1 height:', slide2.offsetHeight);
+console.log('slide1 height:', slide3.offsetHeight);
